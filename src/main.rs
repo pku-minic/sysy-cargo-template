b2789105967974a54143f2b09f4a7f1d31bfb5d9
MaxXSoft <x@MaxXSoft.net>
2021-11-01 12:34:06 +0800

    (main) modified details about 'GENERATOR'

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{Result, Write};
+use lazy_static::lazy_static;
 
-thread_local! {
-  static GENERATOR: HashMap<char, &'static str> = {
+lazy_static! {
+  static ref GENERATOR: HashMap<char, &'static str> = {
     let mut map = HashMap::new();
     map.insert('e', r#"f_main [0]
   return 0
@@ -28,24 +29,22 @@ main:
 fn main() -> Result<()> {
   let mut args = env::args();
   args.next();
-  GENERATOR.with(|g| {
-    match (
-      args.next(),
-      args.next(),
-      args.next(),
-      args.next(),
-      args.next(),
-    ) {
-      (Some(_), Some(m), Some(_), Some(_), Some(f)) if m == "-e" => {
-        write!(File::create(f)?, "{}", g[&'e'])
-      }
-      (Some(_), Some(m), Some(_), Some(_), Some(f)) if m == "-t" => {
-        write!(File::create(f)?, "{}", g[&'t'])
-      }
-      (Some(_), Some(_), Some(_), Some(f), None) => {
-        write!(File::create(f)?, "{}", g[&'r'])
-      }
-      _ => panic!("invalid command line argument"),
+  match (
+    args.next(),
+    args.next(),
+    args.next(),
+    args.next(),
+    args.next(),
+  ) {
+    (Some(_), Some(m), Some(_), Some(_), Some(f)) if m == "-e" => {
+      write!(File::create(f)?, "{}", GENERATOR[&'e'])
     }
-  })
+    (Some(_), Some(m), Some(_), Some(_), Some(f)) if m == "-t" => {
+      write!(File::create(f)?, "{}", GENERATOR[&'t'])
+    }
+    (Some(_), Some(_), Some(_), Some(f), None) => {
+      write!(File::create(f)?, "{}", GENERATOR[&'r'])
+    }
+    _ => panic!("invalid command line argument"),
+  }
 }
